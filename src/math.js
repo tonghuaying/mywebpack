adb4a5e6e8523ccdb1ef4fb15bf4da7e0eb7ca40
tong <906650474@qq.com>
2020-03-14 16:17:39 +0800

    commit

---
+++ src/math.js
@@ -0,0 +1,12 @@
+export function add(a,b) {
+    return a + b;
+}
+export function minus(a,b) {
+    return a - b;
+}
+export function mutil(a,b) {
+    return a * b;
+}
+export function chu(a,b) {
+    return a / b;
+}
